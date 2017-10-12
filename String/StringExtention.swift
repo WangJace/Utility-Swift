7a09a528df5ef070ba99805a81ff777bb779b5ee
caijie <caijie@darma.co>
2017-10-12 17:18:00 +0800

    Date:2017.10.12 Developer:Jace Remark: 删除RegExp.swift，修改StringExtention.swift

--- String/StringExtention.swift
+++ String/StringExtention.swift
@@ -44,6 +44,38 @@ extension String {
         return emailTest.evaluate(with: self)
     }
     
+    // 数字正则表达式
+    func numberRegExp() -> Bool {
+        if self.characters.count == 0 {
+            return false
+        }
+        
+        do {
+            let regex = "[0-9]"
+            let regular = try NSRegularExpression(pattern: regex, options:.caseInsensitive)
+            let results = regular.matches(in: self, options: .reportProgress , range: NSMakeRange(0, self.characters.count))
+            return results.count == self.characters.count
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        return false
+    }
+    
+    // 中文正则表达式
+    func chineseRegExp() -> Bool {
+        if self.characters.count == 0 {
+            return false
+        }
+        
+        for (_, value) in self.characters.enumerated() {
+            if ("\u{4E00}" <= value  && value <= "\u{9FA5}") {
+                return true
+            }
+        }
+        return false
+    }
+    
     //根据开始位置和长度截取字符串
     func subString(start:Int, length:Int = -1) -> String {
         var len = length
